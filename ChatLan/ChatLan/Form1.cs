bdb4712c6e5ea5c0164afbc0b1d3d26f820a12e7
Burak <107427407+buraaks@users.noreply.github.com>
2025-09-23 14:01:52 +0300

    oto ip

--- ChatLan/ChatLan/Form1.cs
+++ ChatLan/ChatLan/Form1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices.JavaScript;
 using System.Windows.Forms;
 
@@ -19,6 +21,54 @@ namespace LanChatApp
             ChatForm chatForm = new ChatForm(isHost, ip);
             chatForm.Show();
             this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtIPNiz.Enabled = true;
+            txtIPGirin.Enabled = false;
+        }
+
+        private void rbHost_CheckedChanged(object sender, EventArgs e)
+        {
+            txtIPNiz.Enabled = true;
+            txtIPGirin.Enabled = false;
+            otoIP.Enabled = true;
+        }
+
+        private void rbClient_CheckedChanged(object sender, EventArgs e)
+        {
+            txtIPNiz.Enabled = false;
+            txtIPGirin.Enabled = true;
+            otoIP.Enabled = false;
+        }
+
+        private string GetLocalIPAddress()
+        {
+            try
+            {
+                string localIP = "";
+                foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork) // IPv4
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+                return localIP;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("IP alınamadı: " + ex.Message);
+                return "";
+            }
+        }
+
+        private void otoIP_Click(object sender, EventArgs e)
+        {
+            string ip = GetLocalIPAddress();
+            txtIPNiz.Text = ip;
         }
     }
 }
